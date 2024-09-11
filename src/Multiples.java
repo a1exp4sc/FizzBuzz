a1626bcea101086b17092c73b7eb92461f24d6ee
Alex Pa <alexpasc@protonmail.com>
2024-09-11 19:45:44 -0400

    refactored to extract helper method

---
+++ src/Multiples.java
@@ -0,0 +1,19 @@
+public class Multiples {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i < 1001; i++) {
+
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                sum += 1;
+            }
+
+        }
+
+        System.out.println(sum);
+    }
+}
